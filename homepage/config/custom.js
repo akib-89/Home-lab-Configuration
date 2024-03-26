71118012cd20a3813b851e0707585e076aa13d83
Akibur Rahman <akib-89@users.noreply.github.com>
2024-03-26 22:59:17 +0600

    Currently homepage looks good now

--- homepage/config/custom.js
+++ homepage/config/custom.js
@@ -10,10 +10,16 @@ bookmarks.forEach(function (bookmark) {
     var link = bookmark.querySelector('a');
     // get the frist chile of the link
     var contaner = link.children[0];
+    // if the container is not null or not a div element
+    if (!contaner || contaner.tagName !== 'DIV') {
+        // continue to the next bookmark
+        return;
+    }
+    // * ok.
     // add a class name to the container element
     contaner.classList.add('bookmark-container');
     // add the theming class to the container
-    contaner.classList.add(...theming);
+    // contaner.classList.add(...theming);
 });
 
 // find all the element with the class bookmark-icon
@@ -27,8 +33,33 @@ bookmarkIcons.forEach(function (bookmarkIcon) {
     bookmarkIcon.classList.remove(...theming);
     // find the only child of the bookmark icon
     var icon = bookmarkIcon.children[0];
+    // if the icon is null 
+    if (!icon) {
+        // continue to the next bookmark icon
+        return;
+    }
     // add remove the w-5, h-5 classed from the icon
     icon.classList.remove('w-5', 'h-5');
     // add the w-12, h-12 class to the icon
     icon.classList.add('w-12', 'h-12', 'bookmark-icon-img');
+});
+
+// find all the elements with class bookmark-list
+var bookmarkLists = document.querySelectorAll('.bookmark-list');
+
+// loop through all the bookmark lists
+bookmarkLists.forEach(function (bookmarkList) {
+    // remove the class name flex-col from the bookmark class list
+    bookmarkList.classList.remove('flex-col');
+    // add the class name flex-row to the bookmark class list
+    bookmarkList.classList.add('flex-row');
+});
+
+// find all the elements with the class bookmark-name
+var bookmarkNames = document.querySelectorAll('.bookmark-name');
+
+// loop through all the bookmark names
+bookmarkNames.forEach(function (bookmarkName) {
+    // remove the pl-3 class from the bookmark name class list
+    bookmarkName.classList.remove('pl-3');
 });
